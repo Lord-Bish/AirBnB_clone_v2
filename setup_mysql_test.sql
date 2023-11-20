31510cbd932514f471b046ead1c6b0739c3f000d
HalimaEla59 <halimaelabbadi@gmail.com>
2023-11-20 11:59:11 -0800

    setup_mysql_test.sql updated

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,7 @@
 -- Prepares MySQL serve, DB hnbn_test_db, user hbnb_test in localhost
 -- Password should be hbnb_test_pwd, hbnb_test must have SELECT+all privileges
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
+SET PASSWORD FOR hbnb_test@localhost = 'hbnb_test_pwd';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hnbn_test'@'localhost';
