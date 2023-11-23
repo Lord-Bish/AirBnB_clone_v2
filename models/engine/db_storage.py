c5f2e8ece8107a41f3e1742d8916127115d89c2b
HalimaEla59 <halimaelabbadi@gmail.com>
2023-11-22 16:51:32 -0800

    ??

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -64,7 +64,7 @@ class DBStorage:
         Base.metadata.create_all(self.__engine)
         ses = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(ses)
-        self.__session = Session()
+        self.__session = Session
 
     def close(self):
         self.__session.close()
